523b5c66cbe5a03e1809025bad5421843d8a83c4
Alan-Rodz <s061199@hotmail.com>
2021-12-28 00:43:45 -0600

    testing, unsafe, compatibility, meta

---
+++ src/compatibility/compatibility1.rs
@@ -0,0 +1,1 @@
+The Rust language is fastly evolving, and because of this certain compatibility issues can arise, despite efforts to ensure forwards-compatibility wherever possible.
---
+++ src/meta/playpen.rs
@@ -0,0 +1,20 @@
+The Rust Playpen is a way to experiment with Rust code through a web interface. This project is now commonly referred to as Rust Playground.
+
+using it with mdbook:
+In mdbook, you can make code examples playable and editable.
+fn main() {
+    println!("Hello World!");
+}
+
+This allows the reader to both run your code sample, but also modify and tweak it. The key here is the adding the word editable to your codefence block separated by a comma.
+```rust,editable
+//...place your code here
+```
+
+Additionally, you can add ignore if you want mdbook to skip your code when it builds and tests.
+```rust,editable,ignore
+//...place your code here
+```
+
+using it with docs:
+You may have noticed in some of the official Rust docs a button that says "Run", which opens the code sample up in a new tab in Rust Playground. This feature is enabled if you use the #[doc] attribute called html_playground_url.
